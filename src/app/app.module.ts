1dc748fb10fd56b5bf184c4cfeed0bdf930f23d8
Ifedayo <karimjnr@gmail.com>
2019-10-19 14:44:08 +0100

    started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParaComponent } from './para/para.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParaComponent
   ],
   imports: [
     BrowserModule,
